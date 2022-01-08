d0ed76c63f8e62c316092bd9042243ff98c74586
rivi <javivarro@msn.com>
2022-01-08 17:17:57 +0100

    Dashboard styles updated

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,11 +3,11 @@ import "./Navbar.css";
 
 export default function NavBar() {
     return (
-        <nav className="Navbar">
-            <div className="Navbar-element-one">
-                <div className="hamburguesa">hamburguesa</div>
-                <div className="hamburguesa">Logo Empresa</div>
-                <div className="hamburguesa">UserName</div>
+        <nav className="navbar">
+            <div className="navbar-elements">
+                <div className="element one">menú desplegable</div>
+                <div className="element two">Logo Empresa</div>
+                <div className="element three">UserName</div>
             </div>
             <select name="shopping-cart" id="shopping-cart-select">
                 <option value="Nueva Cesta">Nueva Cesta</option>
@@ -17,7 +17,6 @@ export default function NavBar() {
                 <NavLink exact to="/" className="" activeClassName="active">Log In</NavLink>
                 <NavLink to="/dashboard" className="" activeClassName="active">Dashboard</NavLink>
             </div>
-
         </nav>
     )
 }
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -14,19 +14,18 @@ export default function Dashboard() {
             <div>
                 <h2>Carrito</h2>
                 <div className="dashboard-section">
-                    <div className="block-section">Carro 1</div>
-                    <div className="block-section">Carro 2</div>
+                    <div className="block-section">Carro: 1</div>
                 </div>
             </div>
             <div>
                 <h2>Ventas</h2>
                 <div className="dashboard-section">
-                    <div className="block-section">Ofertas</div>
-                    <div className="block-section">Pendientes</div>
-                    <div className="block-section">En preparación</div>
-                    <div className="block-section">Preparados</div>
-                    <div className="block-section">Facturas</div>
-                    <div className="block-section">Abonos</div>
+                    <div className="block-section">Ofertas: </div>
+                    <div className="block-section">Pendientes: </div>
+                    <div className="block-section">En preparación: </div>
+                    <div className="block-section">Preparados: </div>
+                    <div className="block-section">Facturas: </div>
+                    <div className="block-section">Abonos: </div>
                 </div>
             </div>
             <div>
