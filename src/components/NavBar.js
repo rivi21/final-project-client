93d65985129587e782f1893a7bc1e421c421c40c
rivi <javivarro@msn.com>
2022-01-08 11:10:19 +0100

    navbar html done. No Css no Js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,23 @@
 import { NavLink } from "react-router-dom";
+import "./Navbar.css";
 
 export default function NavBar() {
     return (
-        <nav className="navbar">
-            <NavLink exact to="/" className="" activeClassName="active">Log In</NavLink>
-            <NavLink to="/dashboard" className="" activeClassName="active">Dashboard</NavLink>
+        <nav className="Navbar">
+            <div className="Navbar-element-one">
+                <div className="hamburguesa">hamburguesa</div>
+                <div className="hamburguesa">Logo Empresa</div>
+                <div className="hamburguesa">UserName</div>
+            </div>
+            <select name="shopping-cart" id="shopping-cart-select">
+                <option value="Nueva Cesta">Nueva Cesta</option>
+                <option value="Cesta 1">Cesta 1</option>
+            </select>
+            <div>
+                <NavLink exact to="/" className="" activeClassName="active">Log In</NavLink>
+                <NavLink to="/dashboard" className="" activeClassName="active">Dashboard</NavLink>
+            </div>
+
         </nav>
     )
 }
