73979246c9be6af7af13a8c10890d8b968886ff8
rivi <javivarro@msn.com>
2022-02-22 15:01:58 +0100

    products table stylized

--- src/Settings.js
+++ src/Settings.js
@@ -1,11 +1,10 @@
 const URL_DUMMY = "https://jsonplaceholder.typicode.com/users";
 
 const URL_GET_CUSTOMERS = "http://localhost/final_project_server/public/index.php/api/customer";
-const URL_GET_INVOICES = "http://localhost/final_project_server/public/index.php/api/invoice"
 const URL_GET_COMISSIONS = "http://localhost/final_project_server/public/index.php/api/comission";
+const URL_GET_INVOICES = "http://localhost/final_project_server/public/index.php/api/invoice";
+const URL_GET_PRODUCTS = 'http://localhost/final_project_server/public/index.php/api/product';
 const URL_POST_LOGIN = 'http://localhost/final_project_server/public/index.php/api/login_check';
-
-
 
 
 export {
@@ -13,6 +12,7 @@ export {
     URL_GET_CUSTOMERS,
     URL_GET_COMISSIONS,
     URL_GET_INVOICES,
+    URL_GET_PRODUCTS,
     URL_POST_LOGIN
 }
 
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { URL_DUMMY } from "../Settings";
+import { URL_GET_PRODUCTS } from "../Settings";
 import SalesForm from "../components/forms/SalesForm";
 import "./FormPages.css";
 
@@ -8,12 +8,12 @@ export default function Products() {
     const [products, setProducts] = useState([]);
 
     useEffect(() => {
-        fetch(URL_DUMMY)
+        fetch(URL_GET_PRODUCTS)
             .then(response => response.json()
-                .then(json => setProducts(json)))
+                .then(data => setProducts(data)))
     }, []);
 
-    let randomQuantity = () => Math.floor(Math.random() * 100 + 1);
+    /* let randomQuantity = () => Math.floor(Math.random() * 100 + 1); */
 
     return (
         <div className="page-title">
@@ -26,19 +26,32 @@ export default function Products() {
                         <th>Producto</th>
                         <th>Cantidad</th>
                         <th>añadir a la cesta</th>
-                    </tr>                    
-                </thead>                
+                    </tr>
+                </thead>
+                
                 <tbody>
                     {products.map((data) => {
                         return (
-                            <tr key={data.id}>
+                            <tr key={data.model}>
                                 <td>Imagen</td>
                                 <td>
                                     <p>{data.id}</p>
-                                    <p>{data.address.zipcode}</p>
-                                    <p>{data.name}</p>
+                                    <p>{data.model}</p>
+                                    <p>{data.type}</p>
                                 </td>
-                                <td>{randomQuantity()}</td>
+                                <td >
+                                    <div className="products-table">
+                                        <p><label htmlFor="unidad">Unidad:</label></p>
+                                        <select name="unidad" id="unidad">
+                                            <option value="Embalaje-12">EMBALAJE (12)</option>
+                                            <option value="Embalaje-24">EMBALAJE (24)</option>
+                                        </select>
+                                    </div>
+                                    <div className="products-table">
+                                        <p><label htmlFor="cantidad">Cantidad:</label></p>
+                                        <input id="cantidad" type="text" ></input>
+                                    </div>
+                                </td>
                                 <td><button>Añadir a la cesta</button></td>
                             </tr>
                         );
